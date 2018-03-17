89d1d59d2f255e47f4479209b7d6cef4a3e9ae48
yinwanng <xxjohnnyng@gmail.com>
2018-03-17 16:41:26 -0700

    counted matrix dimension

--- Assignment1/Assignment1/pagerank.cpp
+++ Assignment1/Assignment1/pagerank.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <Math.h>
 #include "Matrix.h"
 using namespace std;
 
 int main()
 {
+	int inputStringLength = 0;
+	int dimension = 0;
+
 	string filename = "connectivity.txt";
 	ifstream input;
 	input.open(filename);
@@ -16,17 +20,20 @@ int main()
 
 	while (input) {
 		string line;
+	
 		getline(input, line, '\r');
+		
 		int value;
+
 		input >> value;
 
 		cout << line << endl;
+		inputStringLength += line.length();
 	}
+	dimension = sqrt((inputStringLength + 1) / 2);
 
+	cout << "dimension count: " << dimension << endl;
 
-	Matrix m{ 4 };
-
-	cout << m << endl;
 
 	system("pause");
 	return 0;
